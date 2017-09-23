f454924dd1e3188d923b01d03db3506fd6fb9435
liuxining <freeliuxn@gmail.com>
2017-09-23 20:37:45 +0800

    markdown清理

--- app/models.py
+++ app/models.py
@@ -5,7 +5,8 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
-
+from markdown import markdown
+import bleach
 
 class Permission:
     FOLLOW = 0x01
@@ -210,6 +211,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -226,4 +228,15 @@ class Post(db.Model):
                     author=u)
             db.session.add(p)
             db.session.commit()
+
+    @staticmethod
+    def on_change_body(target,value,oldvalue,initiator):
+        allowed_tags = [
+            'a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+            'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+            'h1', 'h2', 'h3', 'p'
+        ]
+        target.body_html = bleach.linkify(bleach.clean(markdown(value,output_format='html'),tags=allowed_tags,strip=True))
+
+db.event.listen(Post.body,'set',Post.on_change_body)
 
