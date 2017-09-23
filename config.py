bbb75c4022a5cceb5b3fda735028721fc832ab03
liuxining <freeliuxn@gmail.com>
2017-09-23 20:39:19 +0800

    123

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
     FLASKY_ADMIN = os.environ.get('MAIL_USERNAME')
+    FLASK_POST_PRE_PAGE = 20
 
     @staticmethod
     def init_app(app):
